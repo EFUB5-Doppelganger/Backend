3ccea7a753ef60b17081ee22d27364957212aba4
9rin <flyrin1208@gmail.com>
2025-11-24 04:25:20 +0900

    feat: Reservation domain 추가

---
+++ src/main/java/com/efub/doppelganger/reservation/Reservation.java
@@ -0,0 +1,44 @@
+package com.efub.doppelganger.reservation;
+
+import com.efub.doppelganger.accommodation.domain.Accommodation;
+import com.efub.doppelganger.member.domain.Member;
+import jakarta.persistence.*;
+import lombok.*;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@EntityListeners(AuditingEntityListener.class)
+public class Reservation {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @CreatedDate
+    private LocalDateTime createdAt;
+
+    // 체크인/체크아웃 날짜
+    private LocalDate checkIn;
+    private LocalDate checkOut;
+
+    private int guests;
+    private int totalPayment;
+
+    // 예약자
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id")
+    private Member member;
+
+    // 어떤 숙소에 대한 예약인지
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "accommodation_id")
+    private Accommodation accommodation;
+
+}
