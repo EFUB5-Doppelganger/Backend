44517913d4d2f0b7c21e0ebc955e787040ec767e
m2nsp <mini071663@gmail.com>
2025-11-29 23:51:04 +0900

    전체 경로 인증 불필요로 변경

--- src/main/java/com/efub/doppelganger/config/SecurityConfig.java
+++ src/main/java/com/efub/doppelganger/config/SecurityConfig.java
@@ -54,8 +54,8 @@ public class SecurityConfig {
                                 "/login/oauth2/**",
                                 "/oauth2/**"
                         ).permitAll()
-                    //나머지는 모두 인증 필요
-                        .anyRequest().authenticated()
+                    // 인증없이 허용
+                        .anyRequest().permitAll()
                 )
 
                 //OAuth2 로그인 설정
